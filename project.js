9141bb5057dae740def3efbb60f24b1cd2188d2d
EunY0511 <bbosong0511@gmail.com>
2021-12-26 21:37:39 +0900

    Title logo and home animation and project code changes

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 // 외부 스크립트는 head에 받아와서 먼저 읽기 때문에, 문서가 준비되면 함수를 실행하라고 지정해줌.
 $(document).ready(function(){
     $('.kakao-project__slider').bxSlider({
-        slideWidth:350,
+        slideWidth:400,
         slideMargin:30,
         prevText:'<i class="fas fa-chevron-left"></i>',
         nextText:'<i class="fas fa-chevron-right"></i>',
@@ -9,21 +9,29 @@ $(document).ready(function(){
     });
 
     $('.toDoList-project__slider').bxSlider({
-        slideWidth:400,
+        slideWidth:500,
         slideMargin:30,
         prevText:'<i class="fas fa-chevron-left"></i>',
         nextText:'<i class="fas fa-chevron-right"></i>',
         pagerType:'short'
     })
 
-    // 드롭다운
-    $("#spreadBtn02").click(function(){
-        if ($("#hiddenList01").is(":visible")){
-            $("#hiddenList01").slideUp(); 
+    // 프로젝트 드롭다운
+    $(".kakao__expandBtn").click(function(){
+        if ($("#kakao__expanded__hidden").is(":visible")){
+            $("#kakao__expanded__hidden").slideUp(); 
         } else {
-            $("#hiddenList01").slideDown(); 
+            $("#kakao__expanded__hidden").slideDown(); 
         } });
 
+    $(".toDoList__expandBtn").click(function(){
+        if ($("#toDoList__expanded__hidden").is(":visible")){
+            $("#toDoList__expanded__hidden").slideUp(); 
+        } else {
+            $("#toDoList__expanded__hidden").slideDown(); 
+        } });
+
+    
 });
 
 
