4558955b2d448429efdd61e01f3342d249723cbe
EunY0511 <bbosong0511@gmail.com>
2021-12-27 23:23:49 +0900

    scroll movement plan

--- project.js
+++ project.js
@@ -30,11 +30,42 @@ $(document).ready(function(){
         } else {
             $("#toDoList__expanded__hidden").slideDown(); 
         } });
-
-    
 });
 
 
+// window.addEventListener("wheel", function(e){
+//     e.preventDefault();
+// },{passive : false});
 
+//  // 스크롤 이동    
+// const btn = document.querySelectorAll('.scrollButton>li');
+// const target_1 = document.querySelector('.a1');
+// const target_2 = document.querySelector('.a2');
+// const target_3 = document.querySelector('.a3');
 
- 
+// const target_dict = {0: 0, 1: target_1, 2: target_2, 3: target_3}
+// var page = 1;
+// var mHtml = $("html");
+
+// mHtml.animate({scrollTop : 0}, 10);
+
+// $(window).on("wheel", function(e) {
+//     if(mHtml.is(":animated")) return;
+//     console.log(e.originalEvent.deltaY)
+//     if(e.originalEvent.deltaY > 0) {
+//         if(page == 4) return;
+//         page++;
+//     } else if(e.originalEvent.deltaY < 0) {
+//         if(page == 1) return;
+//         page--;
+//     }
+//     if(page==1) {
+//         var posTop = 0
+//     } else {
+//         var posTop =(page-1) * $(target_dict[page]).height();
+//     }
+//     // 페이지 높이가 전부 다르기 떄문에 (page-1 *)를 사용할 수 없음
+
+//     console.log(posTop)
+//     mHtml.animate({scrollTop : posTop});
+// })
